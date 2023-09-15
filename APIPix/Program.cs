8c7be93062aed8885d767e5284110eb4e4996ea5
João Victor <joao.silva@detran.ba.gov.br>
2023-09-15 15:08:38 -0300

    [FIX] Configuração de Program para utilização via Swagger

--- APIPix/Program.cs
+++ APIPix/Program.cs
@@ -1,28 +1,42 @@
 using APIPix.IoC;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 {
+    builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddRazorPages();
     builder.Services.AddControllers();
-}
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "Endpoints WEB API PIX",
+            Version = "v1",
+            Description = "Versão 1.0.0.1",
+        });
+    });
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDI(builder.Configuration);
+    builder.Services.AddDI(builder.Configuration);
+}
 
 var app = builder.Build();
 {
+    app.UseHttpsRedirection();
     app.UseRouting();
     app.UseAuthorization();
     app.MapRazorPages();
-    app.MapControllers();
-    app.UseSwagger();
-    app.UseSwaggerUI();
 
-    //if (app.Environment.IsDevelopment())
-    //{
-        
-    //}
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "WEB API PIX v1");
+        c.RoutePrefix = string.Empty;
+    });
+
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers();
+    });
 }
 
 app.Run();
